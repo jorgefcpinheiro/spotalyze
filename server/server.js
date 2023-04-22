feba0019b65ac6ca4dd2e1fca86c1cd1439a68e5
jorgefcpinheiro <al73143@utad.eu>
2023-04-22 15:23:46 +0100

    changed port from 3000 to 5000

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ app.get('/api', (req, res) => {
     res.json({users: ["user1", "user2", "user3"]})
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
 })
